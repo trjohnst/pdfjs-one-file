a19e0b52893147717e94221fd50b24a3ff14ae26
Thomas Johnston <tjohnston@linkedin.com>
2018-02-07 12:17:44 -0800

    Enabling concatenation of the core and worker JS files of pdfjs-dist

---
+++ Brocfile.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const broccoli = require('broccoli');
+const path = require('path');
+const MergeTrees = require('broccoli-merge-trees');
+const concat = require('broccoli-concat');
+const Funnel = require('broccoli-funnel');
+const stew = require('broccoli-stew');
+
+console.log(path.dirname(require.resolve('pdfjs-dist')));
+
+const pdfBuildDir = path.dirname(require.resolve('pdfjs-dist'));
+
+const unminTree = concat(
+  new Funnel(pdfBuildDir, {
+    include: [ 'pdf.js', 'pdf.worker.js' ]
+  }),
+  {
+    outputFile: 'pdf.concat.2-0-334.js',
+    sourceMapConfig: { enabled: false }
+  }
+);
+const minTree = concat(
+  new Funnel(pdfBuildDir, {
+    include: [ 'pdf.min.js', 'pdf.worker.min.js' ]
+  }),
+  {
+    outputFile: 'pdf.concat.min.2-0-334.js',
+    sourceMapConfig: { enabled: false }
+  }
+);
+
+const tree = new MergeTrees([ unminTree, minTree ]);
+
+module.exports = tree;
